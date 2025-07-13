d22a900195332d5a1e22058e160a374739b56331
Yannick Remy <remyyannick@sfr.fr>
2025-07-13 20:00:48 +0200

    feat : connexion à la base de données avec variables d'environnement
    - Création du fichier `db.js` pour gérer la connexion MySQL via mysql2
    - Utilisation du module Dotenv pour lire les variables d'environnement

---
+++ config/db.js
@@ -0,0 +1,20 @@
+const mysql = require("mysql2");
+require('dotenv').config();
+
+const connection = mysql.createConnection({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+});
+
+connection.connect((error) => {
+    if (error) {
+        console.error('Erreur de connexion à la base de données :', error);
+        process.exit(1);
+    } else {
+        console.log('Connecté à la base de données MySQL');
+    }
+});
+
+module.exports = connection;
