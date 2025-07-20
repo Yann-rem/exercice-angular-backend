a8ce01aa420f86b947ebf1fb7d6a54062ee89ddd
Yannick Remy <remyyannick@sfr.fr>
2025-07-20 21:00:21 +0200

    refactor(auth) : déplacement de la logique métier d'authentification
    - Création du fichier `auth.controller.js` pour centraliser la logique d'inscription et de connexion
    - Simplification des routes dans le fichier `auth.routes.js`
    - Mise à jour du fichier `app.js` pour s'aligner sur la nouvelle organisation

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 const express = require('express')
 const cors = require('cors')
-const connection = require('./config/db')
 const conversationRoutes = require('./routes/conversation.routes')
 const authRoutes = require('./routes/auth.routes')
 
@@ -8,8 +7,8 @@ const app = express()
 
 app.use(cors())
 app.use(express.json())
+app.use('/auth', authRoutes)
 app.use('/conversations', conversationRoutes)
-app.use('/auth', authRoutes(connection))
 
 app.get('/', (request, response) => {
   response.send('<h1>Hello depuis app.js !')
--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,75 +1,8 @@
 const express = require('express')
-const bcrypt = require('bcrypt')
-const jwtUtils = require('jsonwebtoken')
-require('dotenv').config()
+const router = express.Router()
+const controller = require('../controllers/auth.controller')
 
-module.exports = (connection) => {
-  const router = express.Router()
+router.post('/register', controller.register)
+router.post('/login', controller.login)
 
-  router.post('/register', (request, response) => {
-    const user = request.body
-    const passwordHash = bcrypt.hashSync(user.password, 10)
-
-    connection.query(
-      'INSERT INTO users(user_email, user_password, role_id, structure_id) VALUES (?, ?, ?, ?)',
-      [user.email, passwordHash, 1, 1],
-      (error, result) => {
-        if (error && error.code === 'ER_DUP_ENTRY') {
-          return response.sendStatus(409)
-        }
-
-        if (error) {
-          console.error(error)
-          return response.sendStatus(500)
-        }
-
-        user.id = result.insertId
-        response.json(user)
-      }
-    )
-  })
-
-  router.post('/login', (request, response) => {
-    const { email, password } = request.body
-
-    connection.query(
-      `
-      SELECT u.user_id, u.user_email, u.user_password, r.role_label
-      FROM users u 
-      JOIN roles r ON u.role_id = r.role_id
-      WHERE u.user_email = ?
-      `,
-      [email],
-      (error, users) => {
-        if (error) {
-          console.error(error)
-          return response.sendStatus(500)
-        }
-
-        if (users.length === 0) {
-          return response.sendStatus(401)
-        }
-
-        const user = users[0]
-        const passwordIsValid = bcrypt.compareSync(password, user.user_password)
-
-        if (!passwordIsValid) {
-          return response.sendStatus(401)
-        }
-
-        const token = jwtUtils.sign(
-          {
-            sub: user.user_email,
-            role: user.role_label,
-            id: user.user_id,
-          },
-          process.env.JWT_SECRET
-        )
-
-        response.json({ token })
-      }
-    )
-  })
-
-  return router
-}
+module.exports = router
